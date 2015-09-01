796730bab91c1163b148ed55f01c8ecb16970e8d
Joe Ainsworth <joe.ainsworth@me.com>
2015-09-01 18:32:13 +0100

    Removed e13.rb

--- Tealeaf Academy - Intro to Programming/exercises/exercise_13.rb
+++
@@ -1,9 +0,0 @@
-people = { "Joe Smith"     => { :email=>"joe@email.com",
-                                :address=>"123 Main st.",
-                                :phone=>"555-123-4567"},
-           "Sally Johnson" => { :email=>"sally@email.com",
-                                :address=>"404 Not Found Dr.",
-                                :phone=>"123-234-3454"} }
-
-puts people["Joe Smith"][:email]
-puts people["Sally Johnson"][:phone]
