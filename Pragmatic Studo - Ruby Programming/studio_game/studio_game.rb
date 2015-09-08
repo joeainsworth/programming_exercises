a6cd36c63a9d072a75e9ffeeb183b671492a62e5
Joe Ainsworth <joe.ainsworth@me.com>
2015-09-08 20:29:42 +0100

    Classes exercises complete

--- Pragmatic Studo - Ruby Programming/studio_game/studio_game.rb
+++ Pragmatic Studo - Ruby Programming/studio_game/studio_game.rb
@@ -1,13 +1,33 @@
-def time
-  current_time = Time.new
-  current_time.strftime("%I:%M:%S")
+class Player
+  def initialize(name, health)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health}."
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
 end
 
-def say_hello(name, health=100)
-  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
-end
+player1 = Player.new("moe", 100)
+puts player1
 
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+player2 = Player.new("Larry", 60)
+puts player2
+
+player3 = Player.new("Curly", 125)
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
