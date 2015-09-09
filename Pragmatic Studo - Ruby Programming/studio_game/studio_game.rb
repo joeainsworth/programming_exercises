ad926a8a03f2e10c70deb60a5fa58b38a2880512
Joe Ainsworth <joe.ainsworth@me.com>
2015-09-09 21:13:14 +0100

    Completed interacting objects exercise

--- Pragmatic Studo - Ruby Programming/studio_game/studio_game.rb
+++ Pragmatic Studo - Ruby Programming/studio_game/studio_game.rb
@@ -1,8 +1,9 @@
 class Player
+
   attr_accessor :name
   attr_reader :health
 
-  def initialize(name, health)
+  def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
@@ -11,39 +12,54 @@ class Player
     "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
-
   def blam
     @health -= 10
     puts "#{@name} got blammed!"
   end
 
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
   def score
     @health + @name.length
+  end
+
+end
+
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(name)
+    @players << name
+  end
+
+  def play
+    puts "There are #{@players.size} players in the game:"
+    @players.each do |player|
+      puts player
+    end
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
   end
 end
 
 player1 = Player.new("moe")
-puts player1
-puts player1.name
-puts player1.health
-puts player1.score
-puts player1
-
 player2 = Player.new("larry", 60)
-puts player2.name
-player2.name = "Lawrence"
-puts player2.name
-puts player2.health
-puts player2.score
-puts player2
-
 player3 = Player.new("curly", 125)
-puts player3
-player3.blam
-player3.w00t
-puts player3
 
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
